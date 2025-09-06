fbea948fd6e4e3b19e6408c1f8156d964697889f
Aditya Nama <adityanama1207@gmail.com>
2025-09-06 16:02:10 +0530

    learning

---
+++ test.js
@@ -0,0 +1,14 @@
+let a = 1
+let b = 2
+let c = 3
+let d = 4
+
+console.log(a, b, c, d)
+console.log(a + b + c + d)
+console.log(a * b * c * d)
+console.log(a / b / c / d)
+console.log(a - b - c - d)
+console.log(a % b % c % d)
+console.log(a ** b ** c ** d)
+console.log(a + b - c * d / a % b ** c)
+console.log((a + b) * (c - d) / (a % b) ** (c ** d))
